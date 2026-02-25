867a54d56f8c81cdab0c3bea246dcc908ae92ff1
dmnlo <d.ivnsvc@gmail.com>
2026-02-25 08:24:29 +0100

    Delete layers directory

--- layers/cover_3.js
+++
@@ -1,1 +0,0 @@
-var json_cover_3 = {"type":"FeatureCollection","name":"cover_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":null}]}
